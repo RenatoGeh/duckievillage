4c04fe1ba7a4c244305d2057b8766da788961353
Renato Lui Geh <renatogeh@gmail.com>
2021-08-04 15:19:47 -0300

    Add (un)installation scripts

---
+++ install.sh
@@ -0,0 +1,87 @@
+# Exits script on error and print offending line.
+function exit_with_error {
+  echo "Error at [${BASH_SOURCE[1]##*/}]->${FUNCNAME[1]}:${BASH_LINENO[0]}"
+  exit 1
+}
+
+# Print help.
+if [[ "$*" == *--help* ]] || [[ "$*" == *-h* ]]; then
+  echo "Usage: $0 [-h|--help] rcfile"
+  echo "  Flags:"
+  echo "    -h | --help  - Prints this help message."
+  echo "  Positional arguments:"
+  echo "    rcfile       - Path to your rc file. If none is given, assume ${HOME}/.bashrc"
+  exit 0
+fi
+
+# Set shell rc path.
+if [ -z "$1" ]; then
+  _shell="${HOME}/.bashrc"
+else
+  _shell="$1"
+fi
+
+printf "Check if Anaconda is installed... "
+if ! command -v conda &> /dev/null; then
+  echo "Anaconda not found! Install anaconda (or miniconda) first."
+  exit 1
+fi
+echo "OK"
+
+printf "Setting up conda.sh... "
+conda_path="$(conda config --show root_prefix | awk '{print $2};')"
+[ ! -z $conda_path ] || exit_with_error
+source "${conda_path}/etc/profile.d/conda.sh" || exit_with_error
+echo "OK"
+
+printf "Check if git is installed... "
+if ! command -v git &> /dev/null; then
+  echo "Git not found! Install git first."
+fi
+echo "OK"
+
+echo "Installing..."
+# First, set the current directory to Duckievillage's.
+pushd . || exit_with_error
+cd "$(dirname "$0")" || exit_with_error
+# Next, clone the Duckietown fork. Skip if directory already exists.
+printf "Check if Duckietown is cloned... "
+if [ ! -d "duckietown" ]; then
+  echo "not cloned."
+  git clone "https://github.com/RenatoGeh/gym-duckietown.git" duckietown || exit_with_error
+else
+  echo "OK"
+fi
+# Create environment for Duckietown.
+echo "Creating environment..."
+conda env create -f duckietown/environment.yaml --name duckietown || exit_with_error
+# Add Duckietown to PYTHONPATH.
+echo "Adding path to ${_shell}..."
+echo "export PYTHONPATH=\"\${PYTHONPATH}:$(pwd)/duckietown\"" >> ${_shell}
+echo "Activating environment..."
+conda activate duckietown || exit_with_error
+# Manually add dependencies which are not in the Anaconda repositories.
+echo "Installing pip dependencies..."
+pip install -r duckietown/requirements.txt || exit_with_error
+echo ""
+echo "---"
+echo "To run Duckievillage, you'll first need to run the following command:"
+echo "       conda activate duckietown"
+echo "This has to be done for every shell session before using Duckievillage."
+echo "---"
+
+# Run test.
+echo "Do you want to run a test to see if everything is working?"
+select opt in "Yes" "No"; do
+  case $opt in
+    Yes ) python3 -m assignments.manual; break;;
+    No ) exit;;
+  esac
+done
+
+# Revert working directory changes.
+popd || exit_with_error
+echo "---"
+echo "All done."
+
+
---
+++ uninstall.sh
@@ -0,0 +1,62 @@
+# Exits script on error and print offending line.
+function exit_with_error {
+  echo "Error at [${BASH_SOURCE[1]##*/}]->${FUNCNAME[1]}:${BASH_LINENO[0]}"
+  exit 1
+}
+
+# Print help.
+if [[ "$*" == *--help* ]] || [[ "$*" == *-h* ]]; then
+  echo "Usage: $0 [-h|--help]"
+  echo "  Flags:"
+  echo "    -h | --help  - Prints this help message."
+  exit 0
+fi
+
+printf "Check if Anaconda is installed... "
+if ! command -v conda &> /dev/null; then
+  echo "Anaconda not found! Install anaconda (or miniconda) first."
+  exit 1
+fi
+echo "OK"
+
+printf "Setting up conda.sh... "
+conda_path="$(conda config --show root_prefix | awk '{print $2};')"
+[ ! -z $conda_path ] || exit_with_error
+source "${conda_path}/etc/profile.d/conda.sh" || exit_with_error
+echo "OK"
+
+echo "Uninstalling..."
+pushd . || exit_with_error
+cd "$(dirname "$0")" || exit_with_error
+if [ -d "duckietown" ]; then
+  echo "Delete the Duckietown repository?"
+  select opt in "Yes" "No"; do
+    case $opt in
+      Yes ) rm -rf ./duckietown; break;;
+      No ) exit;;
+    esac
+  done
+fi
+
+echo "Delete the Anaconda Duckietown environment?"
+select opt in "Yes" "No"; do
+  case $opt in
+    Yes ) conda remove --name duckietown --all; break;;
+    No ) exit;;
+  esac
+done
+
+echo "Clean up unused Anaconda packages?"
+select opt in "Yes" "No"; do
+  case $opt in
+    Yes ) conda clean -a -t -p; break;;
+    No ) exit;;
+  esac
+done
+
+echo "Done!"
+echo "You might want to do some cleaning up on the following:"
+echo "  1. Remove the PYTHONPATH line from your rcfile;"
+echo "  2. Delete this script together with Duckievillage;"
+echo "  3. Completely delete unused Anaconda environment and package files."
+echo "Bye!"
